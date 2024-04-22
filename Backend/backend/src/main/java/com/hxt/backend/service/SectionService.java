2cc8e41b861168a66b935bd103979d78e80d574e
孙永康 <21373154@buaa.edu.cn>
2024-04-22 19:17:00 +0800

    课程中心开发中

---
+++ Backend/backend/src/main/java/com/hxt/backend/service/SectionService.java
@@ -0,0 +1,11 @@
+package com.hxt.backend.service;
+
+import com.hxt.backend.mapper.SectionMapper;
+import jakarta.annotation.Resource;
+
+public class SectionService {
+
+    @Resource
+    private SectionMapper sectionMapper;
+
+}
