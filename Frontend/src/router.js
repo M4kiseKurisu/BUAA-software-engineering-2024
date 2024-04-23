415fe6d48a35c2f454ffd90875c5d3cbe6f47cb1
陈浩然 <21373336@buaa.edu.cn>
2024-04-23 15:20:32 +0800

    changecs

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -76,7 +76,7 @@ const router = createRouter({
                             component: PostCenter,
                         },
                         {
-                            path:'/CourseSection',
+                            path:'/CourseSection/:section_id',
                             component:CourseSection,
                         },
                         {
