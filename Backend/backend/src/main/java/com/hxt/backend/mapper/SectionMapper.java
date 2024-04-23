63b9ef3b224ab83975dd619f9381257e697b8bd9
LBK-miq <2049932883@qq.com>
2024-04-23 17:29:42 +0800

    a

--- Backend/backend/src/main/java/com/hxt/backend/mapper/SectionMapper.java
+++ Backend/backend/src/main/java/com/hxt/backend/mapper/SectionMapper.java
@@ -2,7 +2,9 @@ package com.hxt.backend.mapper;
 
 import com.hxt.backend.entity.section.Section;
 import com.hxt.backend.entity.section.Teacher;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.ArrayList;
@@ -30,4 +32,16 @@ public interface SectionMapper {
 
     @Select("select count(*) from section_follow where section_id = #{id};")
     Integer getFollowCountBySectionId(Integer id);
+
+    //  供管理员使用
+    @Options(useGeneratedKeys = true)
+    @Insert("INSERT INTO section (name, intro, flag, type, academy, credit, capacity) VALUES (#{name}, #{intro}, 0, " +
+            "#{type}, #{academy}, #{credit}, #{capacity})")
+    int insertCourse(String name, String intro, String type,
+                      String academy, Integer credit, Integer capacity);
+
+    @Options(useGeneratedKeys = true)
+    @Insert("INSERT INTO section (name, intro, flag, school_category, web) VALUES (#{name}, #{intro}, 1, " +
+            "#{category}, #{web})")
+    int insertSchool(String name, String intro, String category, String web);
 }
