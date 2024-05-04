267f8e234127634995728a2b686b5735ca2aedbb
M4kiseKurisu <320358301@qq.com>
2024-05-04 13:17:15 +0800

    随便写一下上传打卡界面 fyb

--- Frontend/src/Pages/SocialCenter/SocialPageContainer.vue
+++ Frontend/src/Pages/SocialCenter/SocialPageContainer.vue
@@ -6,11 +6,15 @@
 
         <div class="flex-container">
             <div class="container-left">
-                <div style="margin-left: 5%; margin-bottom: 12px;">
+                <div style="margin-left: 5%; margin-bottom: 12px;" class="flex-container-between">
                     <el-radio-group v-model="radio">
                         <el-radio-button label="查看本人打卡" value="0" />
                         <el-radio-button label="查看打卡广场" value="1" />
                     </el-radio-group>
+
+                    <el-button plain @click="this.dialogTableVisible = true" style="margin-right: 5%;">
+                        发布新打卡
+                    </el-button>
                 </div>
                 <div v-if="radio === '0'"><MySocialPost @childMethod="showDetail"/></div>
                 <div v-if="radio === '1'"><OthersSocialPost /></div>
@@ -19,7 +23,55 @@
             <div class="container-right">
                 <div v-if="this.detail_post_id != 0"><SocialPostDetail :social_post_id="this.detail_post_id"/></div>
             </div>
-        </div>        
+        </div>    
+        
+        
+
+        <!-- 以下为弹出的发布打卡框 -->
+        <el-dialog v-model="dialogTableVisible" title="编辑新打卡" width="75%">
+            <div class="flex-container">
+                <div style="margin-left: 4%; margin-right: 4%; width: 49%; padding-bottom: 12px;">
+                    <div class="title-for-dialog-font" style="margin-top: 15px;">编辑打卡文字</div>
+                    <el-input
+                        v-model="this.social_post_text"
+                        style="width: 100%; margin-top: 16px;"
+                        :autosize="{ minRows: 8, maxRows: 8 }"
+                        type="textarea"
+                        placeholder="输入打卡文字"
+                    />   
+                </div>
+
+                <el-divider direction="vertical"/>
+
+                <div style="width: 36%; margin-right: 4%; margin-left: 3%;">
+                    <div class="title-for-dialog-font" style="margin-top: 15px;">上传打卡图片</div>
+
+                    <div class="flex-container">
+                        <el-upload
+                            class="avatar-uploader"
+                            :show-file-list="false"
+                            style="margin-top: 16px;"
+                        >
+                            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+                        </el-upload>
+
+                        <el-upload
+                            class="avatar-uploader"
+                            :show-file-list="false"
+                            style="margin-top: 16px; margin-left: 16px;"
+                        >
+                            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+                            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+                        </el-upload>
+                    </div>
+                    
+                </div>
+
+            </div>
+
+            <el-button type="primary" style="margin-left: 4%; margin-top: 16px;">上传打卡</el-button>
+        </el-dialog>
 
     </div>
 </template>
@@ -43,6 +95,8 @@ export default {
             route: ["展示模块", "打卡信息"],
             detail_post_id: 0,
             radio: "0",
+            dialogTableVisible: false,
+            social_post_text: "",
         }
     },
     methods: {
@@ -75,5 +129,15 @@ export default {
     border-left: 1px solid #e5e6eb !important;
     margin: 0px !important;
     height: auto !important;
+}
+
+.flex-container-between {
+    display: flex;
+    justify-content: space-between;
+}
+
+.title-for-dialog-font {
+    font-size: 18px;
+    color: #101010;
 }
 </style>
