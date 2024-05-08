7415ad3edab0b7ffe007513be050212b36b68967
QixuanXiang <21373160@buaa.edu.cn>
2024-05-08 16:00:46 +0800

    打卡3.0

--- Backend/backend/src/main/java/com/hxt/backend/mapper/CheckInMapper.java
+++ Backend/backend/src/main/java/com/hxt/backend/mapper/CheckInMapper.java
@@ -39,7 +39,7 @@ public interface CheckInMapper {
     
     //根据打卡id获取打卡图片
     @Select("SELECT image_url from checkin_image WHERE check_in_id = #{checkInId} " +
-            "order by time ASC")
+            "order by cii_id ASC")
     List<String> getImageByCheckInId(Integer checkInId);
     
     //获取打卡所有点赞者id
@@ -73,4 +73,18 @@ public interface CheckInMapper {
     // 更新打卡点赞数
     @Update("UPDATE check_in SET like_count = like_count + #{op} WHERE check_in_id = #{id}")
     int updateCheckInLikeCount(Integer id, Integer op);
+    
+    //评论打卡
+    @Options(useGeneratedKeys = true)
+    @Insert("INSERT INTO checkin_comment (content, author_id, check_in_id, time)" +
+            " VALUES (#{content}, #{authorId}, #{checkInId}, #{time})")
+    int insertCheckInComment(String content, Integer authorId, Integer checkInId, Timestamp time);
+    
+    //删除评论
+    @Delete("DELETE FROM checkin_comment WHERE check_in_id = #{checkInId} and author_id = #{userId}")
+    int deleteCheckInComment(Integer checkInId, Integer userId);
+    
+    //删除打卡
+    @Delete("DELETE FROM check_in WHERE check_in_id = #{checkInId} and author_id = #{userId}")
+    int deleteCheckIn(Integer checkInId, Integer userId);
 }
