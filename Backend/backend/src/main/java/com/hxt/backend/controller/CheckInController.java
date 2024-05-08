b7c284392f691101531fb3b723107115612ba71a
QixuanXiang <21373160@buaa.edu.cn>
2024-05-08 14:24:50 +0800

    部分打卡

--- Backend/backend/src/main/java/com/hxt/backend/controller/CheckInController.java
+++ Backend/backend/src/main/java/com/hxt/backend/controller/CheckInController.java
@@ -1,12 +1,14 @@
 package com.hxt.backend.controller;
 
+import com.hxt.backend.response.BasicInfoResponse;
+import com.hxt.backend.response.checkInResponse.CheckInDetailResponse;
 import com.hxt.backend.response.checkInResponse.CheckInIntroListResponse;
 import com.hxt.backend.response.messageResponse.ChatListResponse;
 import com.hxt.backend.service.CheckInService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,4 +27,38 @@ public class CheckInController {
         //获取pyq列表
         return new CheckInIntroListResponse(checkInService.getPyqList(Integer.parseInt(user_id)));
     }
+    
+    
+    @PostMapping (value = "/pyq/send")
+    public BasicInfoResponse sendCheckIn(
+            @CookieValue(name = "user_id", defaultValue = "") String user_id,
+            @RequestParam(name = "image_urls[]", required = false) List<String> image_urls,
+            @RequestParam(name = "content", required = false) String content
+    ) {
+        if (user_id.equals("")) {
+            return new BasicInfoResponse(false, "用户未登录");
+        }
+        
+        if(image_urls.isEmpty() || content == null) {
+            return new BasicInfoResponse(false, "打卡内容不完整");
+        }
+        
+        Integer res = checkInService.insertCheckIn(Integer.parseInt(user_id), image_urls, content);
+        if (res == 0) {
+            return new BasicInfoResponse(false, "系统出错");
+        }
+        return new BasicInfoResponse(true, "打卡成功");
+    }
+    
+    @GetMapping (value = "/pyq/detail")
+    public CheckInDetailResponse getCheckInDetail(
+            @RequestParam(name = "post_id", required = false) Integer post_id
+    ) {
+        if (post_id == null) {
+            return new CheckInDetailResponse(null);
+        }
+        
+        return checkInService.getCheckInDetail(post_id);
+    }
+    
 }
---
+++ Backend/backend/src/main/java/com/hxt/backend/response/checkInResponse/FavorsInfoResponse.java
@@ -0,0 +1,13 @@
+package com.hxt.backend.response.checkInResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class FavorsInfoResponse {
+    private Integer favor_id;
+    private String favor_avatar;
+}
