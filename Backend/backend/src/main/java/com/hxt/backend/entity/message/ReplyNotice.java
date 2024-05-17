f73456a2f59490881be83672e46ded4671499744
孙永康 <21373154@buaa.edu.cn>
2024-05-17 18:21:21 +0800

    回复通知功能后端完成

--- Backend/backend/src/main/java/com/hxt/backend/entity/message/ReplyNotice.java
+++ Backend/backend/src/main/java/com/hxt/backend/entity/message/ReplyNotice.java
@@ -1,14 +1,29 @@
 package com.hxt.backend.entity.message;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
 
+@Entity
+@Table(name = "reply_notice")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class ReplyNotice {
-
-
+    @Id
+    private Integer rn_id;
+    private Integer user_id;
+    private Integer author_id;
+    private String content;
+    private Timestamp reply_time;
+    private Boolean reply_to_post;
+    private Integer post_id;
+    private Integer comment_id;
 }
